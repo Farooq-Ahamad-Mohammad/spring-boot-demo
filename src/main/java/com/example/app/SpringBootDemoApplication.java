0afc222ea70349bef098861fa49a903651c30d86
farooq <farooqajju1@gmail.com>
2026-06-16 13:02:36 +0530

    stop and run method

--- src/main/java/com/example/app/SpringBootDemoApplication.java
+++ src/main/java/com/example/app/SpringBootDemoApplication.java
@@ -9,5 +9,13 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
+	
+	public void run(String... args) throws Exception {
+		System.out.println("Spring Boot application started successfully!");
+	}
+	
+	public void stop() throws Exception {
+		System.out.println("Spring Boot application stopped successfully!");
+	}
 
 }
